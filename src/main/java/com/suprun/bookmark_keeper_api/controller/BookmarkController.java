27068440d09d6421ba33c0cfafc414a8dd0d738a
yurii-suprun <iursuprun@gmail.com>
2025-11-24 19:58:32 +0100

    Added BookmarkService

---
+++ src/main/java/com/suprun/bookmark_keeper_api/controller/BookmarkController.java
@@ -0,0 +1,14 @@
+package com.suprun.bookmark_keeper_api.controller;
+
+import com.suprun.bookmark_keeper_api.service.BookmarkService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/bookmarks")
+@RequiredArgsConstructor
+public class BookmarkController {
+
+    private final BookmarkService bookmarkService;
+}
