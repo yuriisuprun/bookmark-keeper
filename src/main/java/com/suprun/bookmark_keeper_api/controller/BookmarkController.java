999eba36e677561d2ac58168ecaf02314d380483
yurii-suprun <iursuprun@gmail.com>
2025-11-25 14:28:46 +0100

    Updated BookmarkController

--- src/main/java/com/suprun/bookmark_keeper_api/controller/BookmarkController.java
+++ src/main/java/com/suprun/bookmark_keeper_api/controller/BookmarkController.java
@@ -3,6 +3,7 @@ package com.suprun.bookmark_keeper_api.controller;
 import com.suprun.bookmark_keeper_api.domain.Bookmark;
 import com.suprun.bookmark_keeper_api.service.BookmarkService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ public class BookmarkController {
 
     private final BookmarkService bookmarkService;
 
+    @GetMapping
     public List<Bookmark> getBookmarks() {
         return bookmarkService.getBookmarks();
     }
