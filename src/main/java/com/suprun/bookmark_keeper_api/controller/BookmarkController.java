db9da6600b8fa0b0015053c95c78ac7aeadb2f69
yurii-suprun <iursuprun@gmail.com>
2025-12-01 13:32:53 +0100

    Added pagination

--- src/main/java/com/suprun/bookmark_keeper_api/controller/BookmarkController.java
+++ src/main/java/com/suprun/bookmark_keeper_api/controller/BookmarkController.java
@@ -5,6 +5,7 @@ import com.suprun.bookmark_keeper_api.service.BookmarkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,7 +18,7 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @GetMapping
-    public List<Bookmark> getBookmarks() {
-        return bookmarkService.getBookmarks();
+    public List<Bookmark> getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page) {
+        return bookmarkService.getBookmarks(page);
     }
 }
--- src/main/java/com/suprun/bookmark_keeper_api/service/BookmarkService.java
+++ src/main/java/com/suprun/bookmark_keeper_api/service/BookmarkService.java
@@ -2,6 +2,9 @@ package com.suprun.bookmark_keeper_api.service;
 
 import com.suprun.bookmark_keeper_api.domain.Bookmark;
 import com.suprun.bookmark_keeper_api.repository.BookmarkRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,7 +19,9 @@ public class BookmarkService {
     private final BookmarkRepository bookmarkRepository;
 
     @Transactional(readOnly = true)
-    public List<Bookmark> getBookmarks() {
-        return bookmarkRepository.findAll();
+    public List<Bookmark> getBookmarks(Integer page) {
+        int pageNumber = page < 1 ? 0 : page - 1;
+        Pageable pageable = PageRequest.of(pageNumber, 10, Sort.Direction.DESC, "createdAt");
+        return bookmarkRepository.findAll(pageable).getContent();
     }
 }
