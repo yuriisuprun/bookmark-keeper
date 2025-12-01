4a6336287a579b2e5e07e9fdddc08f035198ec58
yurii-suprun <iursuprun@gmail.com>
2025-12-01 16:15:29 +0100

    Added bookmark dto

--- src/main/java/com/suprun/bookmark_keeper_api/dto/BookmarksDto.java
+++ src/main/java/com/suprun/bookmark_keeper_api/dto/BookmarksDto.java
@@ -1,5 +1,6 @@
 package com.suprun.bookmark_keeper_api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.suprun.bookmark_keeper_api.domain.Bookmark;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,19 +13,21 @@ import java.util.List;
 public class BookmarksDto {
 
     private List<Bookmark> bookmarks;
-    private int totalElements;
+    private long totalElements;
     private int totalPages;
     private int currentPage;
+    @JsonProperty("isFirstPage")
     private boolean isFirstPage;
+    @JsonProperty("isLastPage")
     private boolean isLastPage;
     private boolean hasNextPage;
     private boolean hasPreviousPage;
 
     public BookmarksDto(Page<Bookmark> bookmarkPage) {
         this.setBookmarks(bookmarkPage.getContent());
-        this.setTotalElements(bookmarkPage.getNumberOfElements());
+        this.setTotalElements(bookmarkPage.getTotalElements());
         this.setTotalPages(bookmarkPage.getTotalPages());
-        this.setCurrentPage(bookmarkPage.getNumber());
+        this.setCurrentPage(bookmarkPage.getNumber() + 1);
         this.setFirstPage(bookmarkPage.isFirst());
         this.setLastPage(bookmarkPage.isLast());
         this.setHasNextPage(bookmarkPage.hasNext());
--- src/main/java/com/suprun/bookmark_keeper_api/service/BookmarkService.java
+++ src/main/java/com/suprun/bookmark_keeper_api/service/BookmarkService.java
@@ -9,7 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 @Transactional
