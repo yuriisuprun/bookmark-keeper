38f91952bdf087659b30cbfbfb2ddb7796a52515
yurii-suprun <iursuprun@gmail.com>
2025-12-04 17:15:57 +0100

    Added bookmark dto

---
+++ src/main/java/com/suprun/bookmark_keeper_api/dto/BookmarkDto.java
@@ -0,0 +1,11 @@
+package com.suprun.bookmark_keeper_api.dto;
+
+import java.time.Instant;
+
+public class BookmarkDto {
+
+    private Long id;
+    private String title;
+    private String url;
+    private Instant createdAt;
+}
