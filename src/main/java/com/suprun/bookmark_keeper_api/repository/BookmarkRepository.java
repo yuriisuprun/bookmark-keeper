10ff311e8aeba2fe3f9faff72ef6feabe7a53d51
yurii-suprun <iursuprun@gmail.com>
2026-01-04 19:11:42 +0100

    Updated BookmarkRepository

--- src/main/java/com/suprun/bookmark_keeper_api/repository/BookmarkRepository.java
+++ src/main/java/com/suprun/bookmark_keeper_api/repository/BookmarkRepository.java
@@ -1,9 +1,14 @@
 package com.suprun.bookmark_keeper_api.repository;
 
 import com.suprun.bookmark_keeper_api.domain.Bookmark;
+import com.suprun.bookmark_keeper_api.dto.BookmarkDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
-
+    @Query("select new com.suprun.bookmark_keeper_api.dto.BookmarkDTO(b.id, b.title, b.url, b.createdAt) from Bookmark b")
+    Page<BookmarkDTO> findBookmarks(Pageable pageable);
 }
