0b9a233245cbe1eb5bc708578a60916b59e136f1
yurii-suprun <iursuprun@gmail.com>
2026-01-07 18:33:52 +0100

    Update test for controller with container usage

--- src/test/java/com/suprun/bookmark_keeper_api/controller/BookmarkControllerTest.java
+++ src/test/java/com/suprun/bookmark_keeper_api/controller/BookmarkControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -22,6 +23,9 @@ import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:tc:postgresql:14-alpine:///demo"
+})
 class BookmarkControllerTest {
 
     @Autowired
