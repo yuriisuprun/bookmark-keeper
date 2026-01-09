60f12a3be80c2d967faaf2b9246a6b5e3be0cd10
yurii-suprun <iursuprun@gmail.com>
2026-01-09 16:09:55 +0100

    Updated Bookmark mapper

--- src/main/java/com/suprun/bookmark_keeper_api/mapper/BookmarkMapper.java
+++ src/main/java/com/suprun/bookmark_keeper_api/mapper/BookmarkMapper.java
@@ -8,11 +8,6 @@ import org.springframework.stereotype.Component;
 public class BookmarkMapper {
 
     public BookmarkDTO toDTO(Bookmark bookmark) {
-        BookmarkDTO dto = new BookmarkDTO();
-        dto.setId(bookmark.getId());
-        dto.setTitle(bookmark.getTitle());
-        dto.setUrl(bookmark.getUrl());
-        dto.setCreatedAt(bookmark.getCreatedAt());
-        return dto;
+        return new BookmarkDTO(bookmark.getId(), bookmark.getTitle(), bookmark.getUrl(), bookmark.getCreatedAt());
     }
 }
